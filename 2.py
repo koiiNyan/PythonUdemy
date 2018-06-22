62c3208e0c5a00303a5f3dce8917372656e93594
Anastasiya Ovinnikova <anovinnikova@yandex.ru>
2018-06-22 19:32:11 +0300

    exercises

---
+++ 2.py
@@ -0,0 +1,8 @@
+# Question: What's wrong with the following script?
+
+a = 1 # OK
+_a = 2 # OK
+_a2 = 3 # OK, underscore is fine
+2a = 4 # Wrong (number before name)
+
+# Still all of them are inappropriate as you are meaningless
