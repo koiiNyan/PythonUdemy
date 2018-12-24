c23bdb8a2b510cf0b25b8a0f1f06d34c41a241bd
Anastasiya <37482238+ovinnikova@users.noreply.github.com>
2018-12-24 14:54:46 +0300

    Update and rename 50.py to Section2/50.py

--- Section2/50.py
+++ Section2/50.py
@@ -4,3 +4,5 @@
 age = input("What's your age? ")
 age_last_year = int(age) - 1
 print("Last year you were %s." % age_last_year)
+
+# Input is a str, so to do the maths we need to convert string to int.
